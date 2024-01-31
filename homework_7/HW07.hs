56cb66291683bd813784136b21c8ed5cd429d27c
nikasamadalashvili <65172418+nikasamadalashvili@users.noreply.github.com>
2024-01-31 17:52:10 +0400

    add hw7

--- homework_7/HW07.hs
+++ homework_7/HW07.hs
@@ -4,12 +4,18 @@
 
 module HW07 where
 
-import Cards
-import Control.Monad hiding (liftM, mapM)
+import Cards (Card (..), Deck, labels, suits)
+import Control.Monad (liftM2, replicateM)
 import Control.Monad.Random
-import Data.Functor
-import Data.Monoid
-import Data.Vector (Vector, cons, (!), (!?), (//))
+  ( MonadRandom (getRandom, getRandomR),
+    Rand,
+    Random,
+    StdGen,
+    evalRandIO,
+  )
+import Data.Functor ((<&>))
+import Data.Maybe (fromJust)
+import Data.Vector (Vector, (!), (!?), (//))
 import Data.Vector qualified as V
 import Prelude hiding (mapM)
 
@@ -43,20 +49,25 @@ randomElt v = getRandomR (0, V.length v) <&> (v !?)
 -- Exercise 4 -----------------------------------------
 
 randomVec :: (Random a) => Int -> Rnd (Vector a)
-randomVec = undefined
+randomVec n = V.fromList <$> replicateM n getRandom
 
 randomVecR :: (Random a) => Int -> (a, a) -> Rnd (Vector a)
-randomVecR = undefined
+randomVecR n b = V.fromList <$> replicateM n (getRandomR b)
 
 -- Exercise 5 -----------------------------------------
 
 shuffle :: Vector a -> Rnd (Vector a)
-shuffle = undefined
+shuffle v = V.foldM' (\nv i -> (\j -> fromJust (swapV i j nv)) <$> getRandomR (0, i)) v (V.enumFromStepN vLastIndex (-1) vLastIndex)
+  where
+    vLastIndex = V.length v - 1
 
 -- Exercise 6 -----------------------------------------
 
 partitionAt :: (Ord a) => Vector a -> Int -> (Vector a, a, Vector a)
-partitionAt = undefined
+partitionAt v i = (less, t, greater)
+  where
+    t = v ! i
+    (less, greater) = V.partition (< t) (V.ifilter (\ix _ -> ix /= i) v)
 
 -- Exercise 7 -----------------------------------------
 
@@ -68,36 +79,59 @@ quicksort (x : xs) =
     <> (x : quicksort [y | y <- xs, y >= x])
 
 qsort :: (Ord a) => Vector a -> Vector a
-qsort = undefined
+qsort v
+  | V.null v = V.empty
+  | otherwise = qsort l <> return t <> qsort r
+  where
+    (l, t, r) = partitionAt v 0
 
 -- Exercise 8 -----------------------------------------
 
 qsortR :: (Ord a) => Vector a -> Rnd (Vector a)
-qsortR = undefined
+qsortR v
+  | V.null v = return V.empty
+  | otherwise = do
+      pivot <- getRandomR (0, V.length v - 1)
+      let (l, t, r) = partitionAt v pivot
+      lSorted <- qsortR l
+      rSorted <- qsortR r
+      return $ lSorted <> V.singleton t <> rSorted
 
 -- Exercise 9 -----------------------------------------
 
 -- Selection
 select :: (Ord a) => Int -> Vector a -> Rnd (Maybe a)
-select = undefined
+select i v
+  | i < 0 || i >= V.length v = return Nothing
+  | otherwise =
+      getRandomR (0, V.length v - 1) >>= \pivot ->
+        let (l, t, r) = partitionAt v pivot
+         in case compare i (V.length l) of
+              LT -> select i l
+              GT -> select (i - V.length l - 1) r
+              _ -> return (Just t)
 
 -- Exercise 10 ----------------------------------------
 
 allCards :: Deck
-allCards = undefined
+allCards = [Card l s | s <- suits, l <- labels]
 
 newDeck :: Rnd Deck
-newDeck = undefined
+newDeck = shuffle allCards
 
 -- Exercise 11 ----------------------------------------
 
 nextCard :: Deck -> Maybe (Card, Deck)
-nextCard = undefined
+nextCard = V.uncons
 
 -- Exercise 12 ----------------------------------------
 
 getCards :: Int -> Deck -> Maybe ([Card], Deck)
-getCards = undefined
+getCards 0 d = Just ([], d)
+getCards n d = do
+  (nc, nd) <- nextCard d
+  (cs, rd) <- getCards (n - 1) nd
+  return (nc : cs, rd)
 
 -- Exercise 13 ----------------------------------------
 
